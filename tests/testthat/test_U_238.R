ee084224885f762f012361deadb6e25d09f02a4e
yuanjisun <ysun@eoas.ubc.ca>
2017-11-28 15:54:41 -0800

    update testthat

--- tests/testthat/test_U_238.R
+++ tests/testthat/test_U_238.R
@@ -1,10 +1,10 @@
 context("Test U-238 from Pre-loaded Data")
 
-test_that("Test the accuracy and correctness of U-238 result from the pre-loaded data", {
+test_that("Test correctness of U-238 result from the pre-loaded data", {
         U_actual_1 <- 0.0786*c(1,2,3,4,5,6,7,8,9,10)-0.315
         U_actual_2 <- 0.0713*c(1,2,3,4,5,6,7,8,9,10)
         expect_identical(U_238(test_data,1, plot_it = FALSE),U_actual_1)
         expect_identical(U_238(test_data,2, plot_it = FALSE),U_actual_2)
-        #expect_identical(U_238(test_data,1, plot_it = FALSE),U_actual_2)
-        #expect_identical(U_238(test_data,2, plot_it = FALSE),U_actual_1)
+        expect_failure(expect_equal(U_238(test_data,1, plot_it = FALSE),U_actual_2))
+        expect_failure(expect_equal(U_238(test_data,2, plot_it = FALSE),U_actual_1))
 })
--- tests/testthat/test_compensation_depth.R
+++ tests/testthat/test_compensation_depth.R
@@ -1,7 +1,7 @@
 context("Test Compensation Depth from Pre-loaded Data")
 
-test_that("Test the accuracy and correctness of compensation depth from the pre-loaded data", {
+test_that("Test correctness of compensation depth from the pre-loaded data", {
         expect_identical(compensation_depth(test_data),9)
-        expect_identical(compensation_depth(test_data),10)
-        expect_identical(compensation_depth(test_data),8)
+        expect_failure(expect_equal(compensation_depth(test_data), 10))
+        expect_failure(expect_equal(compensation_depth(test_data), 8))
 })
